cf6e3736bc6abad3da9efab6f1a521f03caa5565
Vlad <vladswork95@gmail.com>
2024-03-08 23:04:17 +0000

    Update

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -2,25 +2,35 @@ const form = document.querySelector(".feedback-form");
 const textarea = form.querySelector("textarea");
 const FORM_STATE = "feedback-form-state";
 
-function formSubmitHandler (event) {
-    event.preventDefault();
+function inputChangeHandler() {
     const email = form.elements.email.value.trim();
     const message = textarea.value.trim();
     const data = JSON.stringify({email, message});
     localStorage.setItem(FORM_STATE, data);
-    form.reset();
 }
 
+function formSubmitHandler (event) {
+    event.preventDefault();
+    const email = form.elements.email.value.trim();
+    const message = textarea.value.trim();
+    const data = JSON.stringify({ email, message });
+    const jsn = localStorage.getItem(FORM_STATE) ?? "";
+    try {
+        const savedData = JSON.parse(jsn);
+        console.log(savedData);
+        if (email !== "" && message !== "" && savedData.email !== undefined && savedData.message !== undefined) {
+            textarea.value = savedData.message;
+            form.elements.email.value = savedData.email;
+            localStorage.removeItem(FORM_STATE);
+            console.log("Form submitted. Received data:", data);
+            form.reset();
+        } else {
+            console.log("Please enter valid data!");
+        }
+    } catch (error) {
+        console.log("Error parsing JSON data:", error);
+    }
+}
 
+form.addEventListener("input", inputChangeHandler);
 form.addEventListener("submit", formSubmitHandler);
-
-const jsn = localStorage.getItem(FORM_STATE) ?? "";
-try {
-    const data = JSON.parse(jsn);
-    console.log(data);
-    textarea.value = data.message;
-    form.elements.email.value = data.email;
-} 
-catch {
-    console.log("Please enter valid data!")
-}
