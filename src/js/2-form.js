51715bb454e8cd6febbf57744de59607f30e8437
Vlad <vladswork95@gmail.com>
2024-03-09 11:19:38 +0000

    Update 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -5,32 +5,45 @@ const FORM_STATE = "feedback-form-state";
 function inputChangeHandler() {
     const email = form.elements.email.value.trim();
     const message = textarea.value.trim();
-    const data = JSON.stringify({email, message});
-    localStorage.setItem(FORM_STATE, data);
+    const data = { email, message }; 
+    localStorage.setItem(FORM_STATE, JSON.stringify(data));
 }
 
-function formSubmitHandler (event) {
+function formSubmitHandler(event) {
     event.preventDefault();
     const email = form.elements.email.value.trim();
     const message = textarea.value.trim();
-    const data = JSON.stringify({ email, message });
+    const data = { email, message };
     const jsn = localStorage.getItem(FORM_STATE) ?? "";
     try {
         const savedData = JSON.parse(jsn);
         console.log(savedData);
-        if (email !== "" && message !== "" && savedData.email !== undefined && savedData.message !== undefined) {
+        if (email !== "" && message !== "") {
             textarea.value = savedData.message;
             form.elements.email.value = savedData.email;
             localStorage.removeItem(FORM_STATE);
             console.log("Form submitted. Received data:", data);
             form.reset();
         } else {
-            console.log("Please enter valid data!");
+            alert("Please fill out both email and message fields:");
         }
     } catch (error) {
         console.log("Error parsing JSON data:", error);
     }
 }
+
+window.addEventListener('load', function() {
+    const jsn = localStorage.getItem(FORM_STATE) ?? "";
+    try {
+        const savedData = JSON.parse(jsn);
+        if (savedData.email !== undefined && savedData.message !== undefined) {
+            textarea.value = savedData.message;
+            form.elements.email.value = savedData.email;
+        }
+    } catch (error) {
+        console.log("Error parsing JSON data:", error);
+    }
+});
 
 form.addEventListener("input", inputChangeHandler);
 form.addEventListener("submit", formSubmitHandler);
